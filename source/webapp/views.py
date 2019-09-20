d05a2c290fbb2afd5b84f3029b24170ccea6ebbb
DonErnest <valeetmihifaveas@gmail.com>
2019-09-20 20:36:44 +0600

    Создание главной страницы(шаблон index.html,index_view в views.py, настройка роутера urls.py)

--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+from webapp.models import Item
+
+
+def index_view(request, *args, **kwargs):
+    items = Item.objects.all()
+    return render(request, 'index.html',context={'items': items})
