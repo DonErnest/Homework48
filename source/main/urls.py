387c9c9f01635321492a503d12721e18f0fae6d2
DonErnest <valeetmihifaveas@gmail.com>
2019-09-23 22:13:56 +0600

    создание страницы просмотра товара item_view.html, настройка роутера, редакция базового шаблона и главной страницы, создание файла forms.py и установка django widgets, добавление виджетов в requirements.txt

--- source/main/urls.py
+++ source/main/urls.py
@@ -2,9 +2,10 @@
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import index_view
+from webapp.views import index_view, item_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', index_view, name = 'main')
+    path('', index_view, name='main'),
+    path('item/<int:pk>/', item_view, name='item_view')
 ]
--- source/webapp/views.py
+++ source/webapp/views.py
@@ -1,8 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.http import HttpResponseBadRequest
 
 from webapp.models import Item
 
 
 def index_view(request, *args, **kwargs):
-    items = Item.objects.all()
+    items = Item.objects.filter(amount__gte=1).order_by('category', 'name')
     return render(request, 'index.html',context={'items': items})
+
+
+def item_view(request, pk):
+    item = get_object_or_404(Item, pk=pk)
+    return render(request, 'item_view.html', context={'item': item})
+
+
+
